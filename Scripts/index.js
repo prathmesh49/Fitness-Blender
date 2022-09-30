3a480742cf06b65b15b6bf419adcda4c35654100
prathmesh49 <prathmesh_kurane@yahoo.com>
2022-09-30 23:01:53 +0530

    add into day4

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,20 @@
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
+document.addEventListener('DOMContentLoaded', function() {
+window.onscroll = function() {myFunction()};
 
-function myFunction(){
-    if(window.pageYOffset >= sticky){
-        navbar.classList.add("sticky");
-    }
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+// console.log('dsdf')
+}
+})
+
+
+function myfun(){
+    console.log("dlfjs")
 }
